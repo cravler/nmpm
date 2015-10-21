940b40cd3a86963f39efdc926b8fb45d3eee9159
cravler <sergeizz@gmail.com>
2015-10-21 09:19:14 +0300

    Improvements

--- index.js
+++ index.js
@@ -4,28 +4,61 @@ var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
 
+var optsToString = function(opts) {
+    var arr = [];
+    for (var key in opts) {
+        if (opts[key]) {
+            var value = '--' + key;
+            if (true !== opts[key]) {
+                value += '=' + opts[key];
+            }
+            arr.push(value);
+        }
+    }
+    return arr.join(' ');
+};
+
 /**
  * @param name
- * @param prefix
+ * @param opts
  * @param filter
  * @constructor
  */
-var Manager = function(name, prefix, filter) {
+var Manager = function(name, opts, filter) {
     var me = this;
 
-    if (typeof prefix === 'function') {
-        filter = prefix;
-        prefix = null;
+    if ('function' === typeof opts) {
+        filter = opts;
+        opts = null;
+    }
+
+    if ('string' === typeof opts) {
+        opts = {
+            prefix: opts
+        };
     }
 
     me._name = name;
-    me._prefix = prefix || process.cwd();
+    me._opts = opts || {
+            'prefix': process.cwd()
+        };
     me._filter = filter || function(pkg) {
-        if (pkg.hasOwnProperty(me._name)) {
-            return true;
-        }
-        return false;
-    };
+            if (pkg.hasOwnProperty(me._name)) {
+                return true;
+            }
+            return false;
+        };
+};
+
+/**
+ * @param name
+ */
+Manager.prototype.require = function(name) {
+    var me = this;
+    if (me._opts['global']) {
+        name = '/usr/local/lib/node_modules/' + name;
+    }
+    return require(name);
 };
 
 /**
@@ -41,12 +74,10 @@ Manager.prototype.info = function(name, callback) {
         try {
             var stat = fs.statSync(name);
             if (stat && stat.isDirectory()) {
-                me.package(name, function(err, pkg) {
-                    if (err) {
-                        return callback(err, null);
-                    }
-                    callback(null, pkg);
-                });
+                var pkg = require(name + '/package.json');
+                if (me._filter(pkg)) {
+                    return callback(null, pkg);
+                }
             } else {
                 callback(null, false);
             }
@@ -74,7 +105,7 @@ Manager.prototype.info = function(name, callback) {
 Manager.prototype.package = function(name, callback) {
     var me = this;
     try {
-        var pkg = require(name + '/package.json');
+        var pkg = me.require(name + '/package.json');
         if (me._filter(pkg)) {
             return callback(null, pkg);
         }
@@ -89,11 +120,11 @@ Manager.prototype.package = function(name, callback) {
  */
 Manager.prototype.list = function(callback) {
     var me = this;
-    exec('npm ls --prefix=' + me._prefix + ' --depth=0 --json', function(err, stdout, stderr) {
+    exec('npm ls ' + optsToString(me._opts) + ' --depth=0 --json', function(err, stdout, stderr) {
         var data = [];
         var dependencies = JSON.parse(stdout)['dependencies'] || {};
         for (var name in dependencies) {
-            var pkg = require(name + '/package.json');
+            var pkg = me.require(name + '/package.json');
             if (me._filter(pkg)) {
                 data.push(name);
             }
@@ -113,7 +144,7 @@ Manager.prototype.install = function(name, callback) {
             return callback(err, null);
         }
         if (pkg) {
-            exec('npm install ' + name + ' --prefix=' + me._prefix + ' --json', function(err, stdout, stderr) {
+            exec('npm install ' + name + ' ' + optsToString(me._opts) + ' --json', function(err, stdout, stderr) {
                 if (err) {
                     return callback(err, null);
                 }
@@ -136,7 +167,7 @@ Manager.prototype.remove = function(name, callback) {
             return callback(err, null);
         }
         if (pkg) {
-            exec('npm remove ' + name + ' --prefix=' + me._prefix + ' --json', function(err, stdout, stderr) {
+            exec('npm remove ' + name + ' ' + optsToString(me._opts) + ' --json', function(err, stdout, stderr) {
                 if (err) {
                     return callback(err, null);
                 }
